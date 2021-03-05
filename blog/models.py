a08e8efb896f40d99f1a15007895ba26027d4d9c
veoco <one@nomox.cn>
2021-03-05 21:39:19 +0800

    add blog models and migrations

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,58 @@
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth import get_user_model
+from django.db import models
 
 
 class User(AbstractUser):
+    pass
+
+
+class BlogMeta(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
+    order = models.IntegerField(default=0)
+
+    class Meta:
+        abstract = True
+
+
+class BlogContent(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+    text = models.TextField()
+    order = models.IntegerField(default=0)
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name='%(class)ss',
+        related_query_name='%(class)s'
+    )
+    template = models.CharField(max_length=255, blank=True, null=True)
+    status = models.CharField(max_length=255)
+    password = models.CharField(max_length=255, blank=True, null=True)
+    allow_comment = models.BooleanField(default=True)
+    allow_ping = models.BooleanField(default=True)
+    allow_feed = models.BooleanField(default=True)
+
+    class Meta:
+        abstract = True
+
+
+class Category(BlogMeta):
+    parent = models.ForeignKey('self', on_delete=models.CASCADE)
+    description = models.CharField(max_length=255)
+
+
+class Tag(BlogMeta):
+    pass
+
+
+class Post(BlogContent):
+    category = models.ManyToManyField(Category, related_name='posts', related_query_name='post')
+    tag = models.ManyToManyField(Tag, related_name='posts', related_query_name='post')
+
+
+class Page(BlogContent):
     pass
