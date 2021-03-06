d5b417062e89c0df54d15d2f18ceb08916795c86
veoco <one@nomox.cn>
2021-03-06 12:53:13 +0800

    add blog models to admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User, Category, Tag, Post, Page
 
 
 admin.site.register(User, UserAdmin)
+admin.site.register(Category)
+admin.site.register(Tag)
+admin.site.register(Post)
+admin.site.register(Page)
