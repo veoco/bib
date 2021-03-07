24d2cbf43a6e43b9f4712605f1264c50502fc9d2
veoco <one@nomox.cn>
2021-03-07 13:35:23 +0800

    add category to base.html header

--- bib/context_processors.py
+++ bib/context_processors.py
@@ -1,5 +1,9 @@
+from blog.models import Category
+
+
 def site_info(request):
     info = {
         'name': '游匿存储'
     }
-    return {'site_info': info}
+    cate = Category.objects.all()
+    return {'site_info': info, 'head_cate': cate}
