676acc6fd2e489521adb5dfae9c47b993388e5c7
veoco <one@nomox.cn>
2021-03-12 19:08:00 +0800

    add feed urls

--- bib/urls.py
+++ bib/urls.py
@@ -18,9 +18,13 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 
+from .feeds import LatestPostFeed, AtomLatestPostFeed
+
 
 urlpatterns = [
     path('', include('blog.urls')),
     path('comments/', include('django_comments.urls')),
     path('admin/', admin.site.urls),
+    path('rss/', LatestPostFeed()),
+    path('atom/', AtomLatestPostFeed()),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
